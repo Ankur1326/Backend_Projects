e0ad53d040341598a6c861226df4202e4c145490
Ankur1326 <swamiankur46@gmail.com>
2023-07-30 11:04:26 +0530

    app.js

--- complete_responsive_dynamic_website/src/app.js
+++ complete_responsive_dynamic_website/src/app.js
@@ -2,15 +2,16 @@ const express = require('express')
 require("./db/conn")
 
 const app = express()
+
 const port = process.env.PORT || 3000
 
-// routing 
+// // routing 
 // app.get( path, callback )
 app.get("/", (req, res) => {
     res.send("Hii by")
 })
 
-// server create 
+// // server create
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 })
