5ffd63f7566be5699704ea6e5370afff7d1abe81
Ankur1326 <swamiankur46@gmail.com>
2023-07-30 11:05:50 +0530

    update conn.js

--- complete_responsive_dynamic_website/src/db/conn.js
+++ complete_responsive_dynamic_website/src/db/conn.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/webThapa', {
+mongoose.connect('mongodb://localhost:27017/DynWebSiteThapa', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
     console.log('Connected to MongoDB');
-  })
+})
   .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+    console.error('Error connecting to MongoDB:', err.message);
+});
