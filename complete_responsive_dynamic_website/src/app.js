bcd5831eb9cc19608e7af40f23d30ddd1e10184a
Ankur1326 <swamiankur46@gmail.com>
2023-08-06 17:44:58 +0530

    update app.js

--- complete_responsive_dynamic_website/src/app.js
+++ complete_responsive_dynamic_website/src/app.js
@@ -5,10 +5,28 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+const path = require("path")
+
+// setting the path
+const staticPath = path.join(__dirname, "../public");
+console.log(staticPath);
+
+// middleware
+app.use("/css", express.static(path.join(__dirname, "../node_modules/bootstarp/dist/css")))
+app.use("/js", express.static(path.join(__dirname, "../node_modules/bootstarp/dist/js")))
+app.use("/css", express.static(path.join(__dirname, "../node_modules/jquery/dist")))
+
+app.set("view engine", "hbs")
+
+
 // // routing 
 // app.get( path, callback )
 app.get("/", (req, res) => {
-    res.send("Hii by")
+    res.render("index")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("contact")
 })
 
 // // server create
